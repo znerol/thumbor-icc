37c4d9ef787743adec119bcd477d87c5163aaaab
Lorenz Schori <lo@znerol.ch>
2015-12-22 15:04:29 +0100

    Fix wrong default for ICC_PATH

--- thumbor_icc/__init__.py
+++ thumbor_icc/__init__.py
@@ -11,5 +11,5 @@ _default_paths = [path for path in _well_known_paths if os.path.exists(path)]
 
 # ICC options
 from thumbor.config import Config
-Config.define('ICC_PATH', None, 'An array of path to directories containing ICC profiles.', 'Color management')
+Config.define('ICC_PATH', _default_paths, 'An array of path to directories containing ICC profiles.', 'Color management')
 Config.define('ICC_DEFAULT_PROFILE', 'sRGB', 'Name of the default ICC profile applied of none is specified.', 'Color management')
